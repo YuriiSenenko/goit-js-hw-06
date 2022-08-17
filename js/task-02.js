80d33ddda361ff5eb8369bff77933c7eed4f75e6
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-17 14:46:43 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,14 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const vegetablesListEl = document.querySelector('#ingredients');
+
+let el = [];
+
+const createElements = ingredients.map((ingredient) => {
+	const newElement = document.createElement('li');
+	newElement.textContent = ingredient;
+	newElement.classList.add('item');
+	el.push(newElement);
+});
+// console.log(el);
+vegetablesListEl.append(...el);
