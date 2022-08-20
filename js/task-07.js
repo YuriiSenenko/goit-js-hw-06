b4581a426184f45429d073ec47836b690868d49b
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-20 13:25:57 +0300

    Task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,3 +2,9 @@ const refs = {
 	inputEl: document.querySelector('#font-size-control'),
 	spanEl: document.querySelector('#text'),
 };
+
+refs.inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+	refs.spanEl.style.fontSize = event.currentTarget.value + 'px';
+}
