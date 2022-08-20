953d7e0ea8590223a6d09f8bb2b8b2b3817f5ee5
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-20 17:26:18 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const refs = {
+	formEl: document.querySelector('.login-form'),
+	inputEmailEl: document.querySelector('input[type=email]'),
+	inputPasswordEl: document.querySelector('input[type=password]'),
+	buttonSubmitEl: document.querySelector('button[type=submit]'),
+};
 
+refs.formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+	const formElements = event.currentTarget.elements;
+
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+
+	if (email === '' || password === '') {
+		return alert('Всі поля повинні бути заповнені!');
+	}
+	console.log({ email: email, password: password });
+	refs.formEl.reset();
+}
