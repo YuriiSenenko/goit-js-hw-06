833094857a40ff446652ff53fbf7924c389f6029
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-21 14:08:53 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body');
+const changeColorButton = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+console.log(body.style);
+
+changeColorButton.addEventListener('click', () => {
+	spanEl.textContent = getRandomHexColor();
+	console.log(spanEl.textContent);
+	body.setAttribute('style', 'background-color:' + spanEl.textContent);
+});
