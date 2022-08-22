6d648286c8c6d4cd50059e00136e4b77946f89c0
YuriiSenenko <yurkosenenko@gmail.com>
2022-08-22 10:09:23 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -5,6 +5,7 @@ const refs = {
 
 refs.inputEl.addEventListener('mousemove', onInputChange);
 
+refs.spanEl.style.fontSize = '56px';
 function onInputChange(event) {
 	refs.spanEl.style.fontSize = event.currentTarget.value + 'px';
 }
